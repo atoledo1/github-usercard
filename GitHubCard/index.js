a7c89ef9489ec38107b796456aac989eedd60d99
adriana <atoledosantiago1@gmail.com>
2020-05-08 04:28:33 -0500

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -8,7 +8,6 @@
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
     Skip to STEP 3.
 */
 
@@ -23,17 +22,13 @@
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -49,7 +44,6 @@ const followersArray = [];
       </div>
     </div>
 */
-
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +52,75 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+const githuInfo = axios.get("https://api.github.com/users/atoledo1");
+githuInfo
+.then(info => {
+cardCreator(info);
+})
+
+.catch(error => {
+console.log(`error: ${error}`);
+})
+
+function cardCreator(info){
+
+const cards = document.querySelector(".cards");
+
+
+const card = document.createElement("div");
+const img = document.createElement("img");
+const cardInfo = document.createElement("div");
+const name = document.createElement("h3");
+const nameUser = document.createElement("p");
+const location = document.createElement("p");
+const linkProfile = document.createElement("p");
+const followers = document.createElement("p");
+const following = document.createElement("p");
+const bio = document.createElement("p");
+
+card.classList.add("card");
+img.classList.add("img");
+cardInfo.classList.add("card");
+name.classList.add("name");
+nameUser.classList.add("username");
+location.classList.add("p");
+linkProfile.classList.add("p");
+followers.classList.add("p");
+following.classList.add("p");
+bio.classList.add("p");
+
+
+img.src =  `${info.data.avatar_url}`;
+name.textContent = `${info.data.name}`;
+nameUser.textContent = `${info.data.login}`;
+location.textContent = `Location: ${info.data.location}`;
+linkProfile.textContent = `Profile: ${info.data.url}`;
+followers.textContent = `Followers: ${info.data.followers}`;
+following.textContent = `Following: ${info.data.following}`;
+bio.textContent = `Bio: ${info.data.bio}`;
+
+card.appendChild(img);
+card.appendChild(cardInfo)
+cardInfo.appendChild(name);
+name.appendChild(nameUser);
+nameUser.appendChild(location);
+location.appendChild(linkProfile);
+linkProfile.appendChild(followers);
+followers.appendChild(following);
+following.appendChild(bio);
+
+return cards.appendChild(card);
+}
+
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+followersArray.forEach(string => { 
+axios.get(`https://api.github.com/users/${string}`)
+.then(info => {
+cardCreator(info);
+})
+.catch(error => {
+console.log(`error: ${error}`);
+})  
+});
